e5f418b2f12fff57f26edf23be333b1f83956e98
SergejsRom <sergejsromanovskis@gmail.com>
2022-12-04 23:13:54 +0200

    migration users, posts

--- project/app/Models/Post.php
+++ project/app/Models/Post.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    protected $fillable = ['',];
+    public function user() 
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
--- project/database/migrations/2022_12_04_170848_create_posts_table.php
+++ project/database/migrations/2022_12_04_170848_create_posts_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('name');
+            $table->text('description');
+            $table->string('status');
             $table->timestamps();
         });
     }
