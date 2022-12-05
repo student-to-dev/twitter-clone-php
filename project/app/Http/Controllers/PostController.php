75946d4fda1f382c4fc102a0452fad5b8401127a
SergejsRom <sergejsromanovskis@gmail.com>
2022-12-05 13:06:21 +0200

    just file postSeeder and index PostController

--- project/app/Http/Controllers/PostController.php
+++ project/app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all()->sortByDesc('created_at');
+        return view('index', compact('posts'));
     }
 
     /**
---
+++ project/database/seeders/PostSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+ 
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Database\Seeders\Faker\Factory;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0; $i < 20; $i++) { 
+            $faker = Faker\Factory::create(Arr::random());
+            DB::table('posts')->insert([
+    
+              
+                
+            ]);
+        }
+    
+    }
+}
