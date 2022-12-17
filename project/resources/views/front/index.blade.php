61f9c7e3e9030417b7857b7e2329cc547fe05173
SergejsRom <sergejsromanovskis@gmail.com>
2022-12-18 00:20:28 +0200

    separate layouts and navigation menus

--- project/resources/views/front/index.blade.php
+++ project/resources/views/front/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-user-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('All posts') }}
@@ -12,4 +12,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-user-layout>
--- project/resources/views/navigation-menu-admin.blade.php
+++ project/resources/views/navigation-menu-admin.blade.php
@@ -3,7 +3,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
+                <!-- Logo --> 
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('all-posts') }}">
                         <x-jet-application-mark class="block h-9 w-auto" />
