4a880a3c6b090a49c2bd5a9977a16edae3a80f02
SergejsRom <sergejsromanovskis@gmail.com>
2022-12-17 23:26:48 +0200

    login redirect to all-posts

--- project/routes/web.php
+++ project/routes/web.php
@@ -25,9 +25,9 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-return view('dashboard');
-    })->name('dashboard');
+//     Route::get('/dashboard', function () {
+// return view('dashboard');
+//     })->name('dashboard');
     
 });
 
