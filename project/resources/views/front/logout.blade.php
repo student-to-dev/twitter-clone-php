48dee2fd4cee59fa29836bdd0f4af70dccb1f491
SergejsRom <sergejsromanovskis@gmail.com>
2022-12-27 19:46:41 +0200

    .

--- project/resources/views/front/logout.blade.php
+++ project/resources/views/front/logout.blade.php
@@ -23,7 +23,7 @@
                 </a>
                 </form>
                 
-                    <a class="inline-block px-6 py-2.5 bg-white text-black-700 font-medium text-s leading-tight rounded-full shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-32 text-center font-bold"
+                    <a class="inline-block px-6 py-2.5 bg-[#29a8df] text-black-700 font-medium text-s leading-tight rounded-full shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out w-32 text-center font-bold"
                      href="{{ route('all-posts') }}">Go back</a>
                 </div>
             </div>
