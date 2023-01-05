e035c981633ccf64c3d64e8b758395ce2379ab74
SergejsRom <sergejsromanovskis@gmail.com>
2023-01-05 03:01:19 +0200

    name

--- project/resources/views/front/show.blade.php
+++ project/resources/views/front/show.blade.php
@@ -10,11 +10,11 @@
             <div class="bg-[#14202E] overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="flex justify-center mb-2 rounded-md">
                     <div class="block p-6 rounded-lg shadow-lg bg-[#1F2C3C] w-64 max-w-sm md:max-w-lg sm:min-w-[50%]">
-                        <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2">{{ $post->user->name }}</h5>
+                        <h5 class="text-gray-100 text-xl leading-tight font-medium mb-2 truncate">{{ $post->user->name }}</h5>
                         <p class="text-gray-100 text-md mb-4 break-words bg-[#253243] p-3 rounded-md">
                             {{ $post->description }}
                         </p>
-                            <img class="rounded-lg" src="{{ asset("../storage/app/$post->image") }}" alt="">
+                            <img class="rounded-lg" src="{{ asset("../storage/app/$post->image") }}" alt="#">
                     </div>
                 </div>   
             </div>
