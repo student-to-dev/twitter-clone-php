892b4540e5500c999bd009f4d51bc683140a17b1
SergejsRom <sergejsromanovskis@gmail.com>
2023-01-05 20:18:31 +0200

    .

--- project/app/Actions/Fortify/CreateNewUser.php
+++ project/app/Actions/Fortify/CreateNewUser.php
@@ -26,7 +26,7 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-        ], ['alpha_dash' => 'The useer name must be a single word'])->validate();
+        ], ['alpha_dash' => 'The user name must be a single word'])->validate();
 
         return User::create([
             'name' => $input['name'],
