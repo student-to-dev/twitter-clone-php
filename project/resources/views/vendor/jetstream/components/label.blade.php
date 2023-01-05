6400cf58399777a921fc0c09190a830cc6628463
SergejsRom <sergejsromanovskis@gmail.com>
2023-01-05 02:01:02 +0200

    added if on request to show color

--- project/resources/views/vendor/jetstream/components/label.blade.php
+++ project/resources/views/vendor/jetstream/components/label.blade.php
@@ -1,7 +1,7 @@
 @props(['value'])
 
 <label 
-@if (Request::is('user/profile'))
+@if (Request::is('user/profile') || Request::is('livewire/message/profile.update-profile-information-form'))
 {{ $attributes->merge(['class' => 'block font-medium text-gray-500 text-lg']) }}
 @endif
 {{ $attributes->merge(['class' => 'block font-medium text-white text-lg']) }}
